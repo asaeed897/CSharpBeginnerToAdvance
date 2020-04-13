5b56018cc8c10b9f9a276da724daa1ef1625fcb1
awais <asaeed897>
2020-04-13 19:28:17 +0500

    Lession 17 and 18 completed

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -6,11 +6,19 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
+            const float Pi = 3.14f;
             byte num = 255;
-            Console.WriteLine("Before " + num);
-            num = (byte)(num + 1);
-            Console.WriteLine("After " + num);
-            //Overflow
+            int num2 = 34;
+            char Char = 'S';
+            string name = "Awais";
+            Console.WriteLine(num);
+            Console.WriteLine(num2);
+            Console.WriteLine(Char);
+            Console.WriteLine(name);
+            Console.WriteLine("Range of byte: {0} {1}",byte.MinValue, byte.MaxValue);
+            Console.WriteLine("Range of Int: {0} {1}",int.MinValue,int.MaxValue );
+            Console.WriteLine(Pi);
+            // Variables and Constants
         }
     }
 }
