63b6f41a43f4aaa18093c7a6f9b7ec95920504a7
awais <asaeed897>
2020-04-13 20:28:52 +0500

    Completed Section 3

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -6,13 +6,9 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            int a = 2;
-            int b = 4;
-            int c = 7;
-            Console.WriteLine(a+b);
-            Console.WriteLine(b/a);
-            Console.WriteLine(c>b && b>a);
-            //21,22,23 and 24. Operators and comments
+            int number1 = 257;
+            byte number2 = number1;
+            // Quiz 2 completed
         }
     }
 }
