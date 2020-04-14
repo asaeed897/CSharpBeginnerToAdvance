0b4b21be340091c30f554e72f7286b61faaae5e9
awais <asaeed897>
2020-04-14 22:16:51 +0500

    Compeleted Lession 29 Structs

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,24 +1,30 @@
 ﻿
 
 using System;
-using CSharpBeginner.Math;
 
 namespace CSharpBeginner
 {
+    public struct Calculator
+    {
+        public int num1;
+        public int num2;
+
+        public int add()
+        {
+            return num1 + num2;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var awais = new Person();
-            awais.firstName = "Awais";
-            awais.lastName = "Saeed";
-            awais.Indtroduction();
+            
             var cal = new Calculator();
             cal.num1 = 4;
             cal.num2 = 5;
             int sum = cal.add();
             Console.WriteLine("Sum: " +sum);
-            // 26, 27 and 28 Classes 
+            // 29 Structs 
         }
     }
 }
