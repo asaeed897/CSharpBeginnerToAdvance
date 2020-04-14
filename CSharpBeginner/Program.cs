9bb2039f25d73a5e7e334aa0340a00bfd332a6d5
awais <asaeed897>
2020-04-15 00:18:11 +0500

    Completed Lession 34 and 35 Enums

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,40 +1,31 @@
-﻿
-
-using System;
+﻿using System;
 
 namespace CSharpBeginner
 {
-    public struct Calculator
+    public enum ShippingMethods
     {
-        public int num1;
-        public int num2;
-
-        public int add()
-        {
-            return num1 + num2;
-        }
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var firstName = "Awais";
-            var lastName = "Saeed";
-            var fullName = firstName + " " + lastName; ;
-            Console.WriteLine(fullName);
+            var method = ShippingMethods.Express;
+            Console.WriteLine((int)method);
 
-            var myFullName = string.Format("My name is {0} {1}",firstName,lastName);
-            Console.WriteLine(myFullName);
+            var methodId = 2;
+            Console.WriteLine((ShippingMethods) methodId) ;
 
-            var names = new string[3] {"Asim", "Awais", "Mustensar"};
-            var formattedNames = string.Join(",", names);
-            Console.WriteLine(formattedNames);
+            var methodString = method.ToString();
+            Console.WriteLine(methodString);
 
-            var line = @"Hello,
- My name is Awais";
-            Console.WriteLine(line);
-
-            // 32 and 33 Strings 
+            var methodName = "Express";
+            var shippingMethods = (ShippingMethods)Enum.Parse(typeof(ShippingMethods), methodName);
+            Console.WriteLine(shippingMethods); 
+            // 34 and 35 Enums
         }
+
     }
-}
+} 
