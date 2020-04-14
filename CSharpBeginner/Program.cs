e7e9a0938429175dd80b3fa1ffebb1cabf4c9de3
awais <asaeed897>
2020-04-14 22:55:42 +0500

    Completed Lession 30 and 31 Arrays

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -19,12 +19,17 @@ namespace CSharpBeginner
         static void Main(string[] args)
         {
             
-            var cal = new Calculator();
-            cal.num1 = 4;
-            cal.num2 = 5;
-            int sum = cal.add();
-            Console.WriteLine("Sum: " +sum);
-            // 29 Structs 
+            var numbers = new int[3] {1,2,3};
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var names = new string[3] {"Awais", "Mustansar", "Asim"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+            // 30 and 31 Arrays 
         }
     }
 }
