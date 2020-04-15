1d5c69b44d850d2833d2b7bd0deb6ea388774d3c
awais <asaeed897>
2020-04-15 23:25:18 +0500

    Completed Lessions 36, 37 and 38 Reference Types aand Value Types

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,31 +1,45 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace CSharpBeginner
 {
-    public enum ShippingMethods
+    public class Person
     {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
+        public int Age;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            var method = ShippingMethods.Express;
-            Console.WriteLine((int)method);
+            var a = 5;
+            var b = a;
+            b++;
+            Console.WriteLine("a: {0}, b: {1}", a,b);
 
-            var methodId = 2;
-            Console.WriteLine((ShippingMethods) methodId) ;
+            var arry1 = new int[3] {1, 2, 3};
+            var arry2 = arry1;
+            arry2[0] = 0;
+            Console.WriteLine("arry1: {0}, arry2: {1}", arry1[0],arry2[0]);
 
-            var methodString = method.ToString();
-            Console.WriteLine(methodString);
+            // 36, 37 and 38 Reference Types and Value Types
 
-            var methodName = "Express";
-            var shippingMethods = (ShippingMethods)Enum.Parse(typeof(ShippingMethods), methodName);
-            Console.WriteLine(shippingMethods); 
-            // 34 and 35 Enums
+            var number = 5;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
         }
 
+        public static void Increment(int number)
+        {
+            number++;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
     }
 } 
