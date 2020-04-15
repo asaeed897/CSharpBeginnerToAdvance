31597285735771d287b1f0581c661b0b9ab69349
awais <asaeed897>
2020-04-16 00:18:33 +0500

    Completed Lession 41, 42 and 43 If/Else and Switch Statements

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -3,16 +3,48 @@ using System.Security.Cryptography;
 
 namespace CSharpBeginner
 {
-    
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Winter,
+        Autumn
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            
 
-            // 40 Introduction Control Flow
+            var num = 10;
+            if (num > 10)
+            {
+                Console.WriteLine("Given number is greater than 10");
+            }
+            else if (num == 10)
+            {
+                Console.WriteLine("Given number is equal to 10");
+            }
+            else
+            {
+                Console.WriteLine("Given number is less than 10");
+            }
 
-     
+            switch (Season.Spring)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's a beautiful season.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's known for vacations");
+                    break;
+
+                default:
+                    Console.WriteLine("Stay Home, Stay Safe. It's Corona Season!!!");
+                    break;
+            }
+            // 41, 42 and 43 If/Else and Switch statements
+
+
         }
 
     }
