a78e089e04c4227bbc7285bc6772318f1f979088
awais <asaeed897>
2020-04-16 23:55:06 +0500

    Completed Lesson 61 Summary Section 7 Completed Working With Dates

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -8,32 +8,8 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            //Creating
-            var timeSpan = new TimeSpan(1,2,3);
-            var timeSpan1 = TimeSpan.FromHours(1);
-
-            var start = DateTime.Now;
-            var end = start.AddMinutes(2);
-            var duration = end - start; // Here duration id become a timeSpan object
-            Console.WriteLine("Duration: "+ duration);
-
-            //Properties
-            Console.WriteLine("Minutes: "+ timeSpan.Minutes);
-            Console.WriteLine("TotalMinutes: "+ timeSpan.TotalMinutes);
-
-            //Add
-            Console.WriteLine("Add Example: "+ timeSpan.Add(TimeSpan.FromMinutes(8)));
-
-            //Subtract 
-            Console.WriteLine("Subtract: "+ timeSpan.Subtract(TimeSpan.FromMinutes(2)));
-
-            //ToString
-            Console.WriteLine("ToString: "+ timeSpan.ToString());
-
-            //Parse
-            Console.WriteLine("Parse: "+ TimeSpan.Parse("01:05:20"));
-
-            // 60. TimeSpan
+            
+            // 61. Quiz and Summary
         }
 
 
