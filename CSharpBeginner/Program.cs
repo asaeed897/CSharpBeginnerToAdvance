a46cd6122f2af60a3491916007dfb808e8a0116d
awais <asaeed897>
2020-04-16 19:39:10 +0500

    Completed Lession 47 While Loop

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -8,23 +8,21 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            var name = "Awais Saeed";
-
-            Console.WriteLine("Using Foor Loop");
-            for (var i = 0; i < name.Length; i++)
+            while (true)
             {
-                Console.WriteLine(name[i]);
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo "+ input);
+                    continue;
+                }
+
+                break;
             }
 
-            Console.WriteLine("Using Foreach LOOP");
 
-            foreach (var character in name)
-            {
-                Console.WriteLine(character);
-            }
-
-
-            // 45 and 46 For Loop and foreach Loop
+            // 47 While Loop 
 
 
         }
