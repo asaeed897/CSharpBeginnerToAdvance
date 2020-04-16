1ab85e04c43d000c9b6f3a5a5a003afab55a9cd9
awais <asaeed897>
2020-04-16 22:24:18 +0500

    Completed Lession 54 and 55 Lists

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.Collections.Generic;
 
 namespace CSharpBeginner
 {
@@ -8,48 +8,40 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            var numbers = new[] {1, 2, 7, 8, 3, 4, 5};
-            //Length
-            Console.WriteLine("Length of array: "+ numbers.Length);
+            var numbers = new List<int>() {1, 2, 3, 5, 6};
 
-            //IndexOf
-            Console.WriteLine("Index of 7: "+Array.IndexOf(numbers, 7));
-
-            //Clear()
-            Array.Clear(numbers,2, 2);
-            Console.WriteLine("Effects of Clear");
-            foreach (var VARIABLE in numbers)
+            numbers.Add(1);
+            numbers.AddRange(new int[3]{7, 8, 9});
+            foreach (var number in numbers)
             {
-                Console.WriteLine(VARIABLE);
+                Console.WriteLine(number);
             }
 
-            //Copy()
-            var another = new int[4];
-            Array.Copy(numbers, another, 4);
-            Console.WriteLine("Another Array: ");
-            foreach (var VARIABLE in another)
+            Console.WriteLine();
+            Console.WriteLine("Index of 1 is "+ numbers.IndexOf(1));
+            Console.WriteLine("Index of 1 is "+ numbers.LastIndexOf(1));
+
+            Console.WriteLine("Count: "+numbers.Count);
+
+            for (int i = 0; i < numbers.Count; i++)
             {
-                Console.WriteLine(VARIABLE);
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);  
             }
             
-            //Sort()
-            Array.Sort(numbers);
-            Console.WriteLine("Effects of Sort");
-            foreach (var VARIABLE in numbers)
-            {
-                Console.WriteLine(VARIABLE);
-            }
-
-            //Reverse()
-             Array.Reverse(numbers);
-             Console.WriteLine("Effects of Reverse");
-             foreach (var VARIABLE in numbers)
-             {
-                 Console.WriteLine(VARIABLE);
-             }
-            // 51, 52 and 53 Introduction and Arrays
+            numbers.Clear();
+            Console.WriteLine("Count: "+numbers.Count);
+            // 54 and 55 Lists
 
         }
+
 
     }
 } 
