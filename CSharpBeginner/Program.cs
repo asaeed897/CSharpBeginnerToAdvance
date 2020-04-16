874f82c5beed8a2a7a3898d2958741b37fcbb630
awais <asaeed897>
2020-04-16 23:00:08 +0500

    Completed Lesson 58 Introduction

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -9,30 +9,8 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            Console.Write("Write your name: ");
-            var name = Console.ReadLine();
-
-            var nameArray = new char[name.Length];
-            // Alternate method
-            /*for (int i = 0; i < name.Length; i++)
-            {
-                nameArray[i] = name[i];
-            }
-
-            Array.Reverse(nameArray);*/
             
-            var j = 0;
-            for (int i = name.Length -1 ; i >=0; i--)
-            {
-                nameArray[j] = name[i];
-                j++;
-            }
-
-            foreach (var Character in nameArray)
-            {
-                Console.Write(Character);
-            }
-            // 56 and  57 Exercise and Summary
+            // 58 Introduction Section 7 Working with DateTime
 
         }
 
