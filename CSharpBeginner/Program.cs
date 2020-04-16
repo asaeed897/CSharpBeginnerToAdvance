80c636cd422fc8dbe2c7c315409dc4a0f3db2064
awais <asaeed897>
2020-04-16 18:51:02 +0500

    Completed Lession 44 Iteration Statements

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -3,25 +3,14 @@ using System.Security.Cryptography;
 
 namespace CSharpBeginner
 {
-    public enum ImageOrientation
-    {
-        Landscape,
-        Portrait
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter Image Width: ");
-            var width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Image Height");
-
-            var height = Convert.ToInt32(Console.ReadLine());
-
-            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
-            Console.WriteLine("Image orientation is "+ orientation);
-            // 43 If/Else and Switch statements Exercise
+            
+            // 44 Iteration statements
 
 
         }
