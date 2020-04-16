1fc26148a84dab12e91ffcbb25d5f76a1fdf708a
awais <asaeed897>
2020-04-16 23:21:24 +0500

    Completed Lesson 59 DateTime

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace CSharpBeginner
 {
@@ -9,8 +8,24 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            
-            // 58 Introduction Section 7 Working with DateTime
+            var Date = new DateTime(2020,01,22);
+            Console.WriteLine(Date);
+
+            var now = DateTime.Now;
+            /*Console.WriteLine(now.Day);
+            Console.WriteLine(now.Hour);
+            Console.WriteLine(now.Minute);*/
+
+            now = now.AddDays(1);
+            Console.WriteLine(now.Day);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("dd-MM-yyyy"));
+
+            // 59 DateTime
 
         }
 
