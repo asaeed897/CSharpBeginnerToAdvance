954f92fbe0d680783beb1fb3b04dfea9a51d6330
awais <asaeed897>
2020-04-17 22:50:54 +0500

    Completed Lesson 75, 76 and 77 Path Class & Summary Section 9 Completed

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -11,29 +11,17 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Users\awais\Downloads\Video";
+            var path = @"C:\Users\awais\Downloads\Video\image.jpg";
 
-            Directory.CreateDirectory(@"c:\folder");
-            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
-            /*foreach (var file in files)
-            {
-                Console.WriteLine(file);
-            }*/
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
 
-            var directories = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
-            foreach (var directory in directories)
-            {
-                Console.WriteLine(directory);
-            }
+            Console.WriteLine("Extension: "+Path.GetExtension(path));
+            Console.WriteLine("File Name: "+Path.GetFileName(path));
+            Console.WriteLine("File Name without Extension: "+Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: "+Path.GetDirectoryName(path));
 
-            Directory.Exists(path);
-
-            var directoryInfo = new DirectoryInfo(path);
-            directoryInfo.GetFiles();
-            directoryInfo.GetDirectories();
-
-
-            // 74. Directory and DirectoryInfo
+            // 75, 76, and 77. Path class & Summary
         }
 
     }
