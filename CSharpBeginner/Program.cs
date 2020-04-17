52d4ae600078b6a8bc34cf6bfef066dd4c69673d
awais <asaeed897>
2020-04-17 16:59:24 +0500

    Completed  Lesson 65 Sumarizing Text

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
 namespace CSharpBeginner
@@ -8,29 +9,13 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            var fullName = "Awais Saeed ";
-            fullName = fullName.Trim();
-            Console.WriteLine("Trim: '{0}'",fullName);
+            var sentence = "My name is Muhammad Awais Saeed and today I am feeling bore";
 
-            var index = fullName.IndexOf(" ");
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("FirstName: "+firstName);
-            Console.WriteLine("LastName: "+lastName);
-
-            Console.WriteLine("ToUpper: "+ fullName.ToUpper());
-
-            var name = fullName.Split(' ');
-            Console.WriteLine("FirstName: " + name[0]);
-            Console.WriteLine("LastName: " + name[1]);
-
-            var str = "534";
-            var num = int.Parse(str);
-            var num1 = Convert.ToInt32(str);
-
-            Console.WriteLine("Parse: {0}\nConvertToInt32: {1}",num,num1);
-            // 62, 63 and 64. Introduction and Strings
+            var summary = StringUtility.SummarizeText(sentence);
+            Console.WriteLine(summary);
+            // 65. Live Coding Summarizing Text
         }
+
 
 
     }
---
+++ CSharpBeginner/StringUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBeginner
+{
+    class StringUtility
+    {
+        public static string SummarizeText(String text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+
+            var words = text.Split(" ");
+            var wordsList = new List<string>();
+
+            var characterCount = 0;
+            foreach (var word in words)
+            {
+                characterCount += word.Length;
+                if (characterCount > maxLength)
+                {
+                    break;
+                }
+                wordsList.Add(word);
+            }
+
+            return String.Join(" ", wordsList) + "...";
+
+
+
+        }
+
+    }
+}
