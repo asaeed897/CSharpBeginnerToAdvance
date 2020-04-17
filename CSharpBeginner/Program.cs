4c0ac759a5088d7cbf7d0eb76c39978066e114ba
awais <asaeed897>
2020-04-17 17:34:15 +0500

    Completed Lesson 66 and 67 String Builder

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 
 namespace CSharpBeginner
@@ -9,11 +10,20 @@ namespace CSharpBeginner
     {
         static void Main(string[] args)
         {
-            var sentence = "My name is Muhammad Awais Saeed and today I am feeling bore";
+            var builder  = new StringBuilder();
 
-            var summary = StringUtility.SummarizeText(sentence);
-            Console.WriteLine(summary);
-            // 65. Live Coding Summarizing Text
+            builder
+                .Append('+', 10)
+                .AppendLine()
+                .Append("Awais Saeed")
+                .AppendLine()
+                .Append('+', 10)
+                .Replace('+', '-')
+                .Remove(0,10)
+                .Insert(0, new string("Name: "));
+            Console.WriteLine(builder);
+            Console.WriteLine("First Char: "+builder[0]);
+            // 66 and 67. StringBuilder
         }
 
 
