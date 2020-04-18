dadac6c99bb386f8a4f2ae942f97f2c8d34a158e
awais <asaeed897>
2020-04-18 22:18:33 +0500

    Completed Lesson 7 Introduction to Classes

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -2,12 +2,31 @@
 
 namespace CSharpIntermediate
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string toName)
+        {
+            Console.WriteLine("Hi {0}, I am {1}",toName,Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            return new Person() {Name = str};
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Star CSharp Intermediate Course!");
-            // Section 1 is About the Course Lessons 1 to 6
+            //var person = new Person();
+           // person.Name = "Awais";
+            //person.Introduce("Asim");
+
+            var person = Person.Parse("Awais");
+            person.Introduce("Asim");
+            // 7 Introduction to Classes
         }
     }
 }
