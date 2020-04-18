0daa57b5f4582dfb39ee4cd062c0ceaf742bd40f
awais <asaeed897>
2020-04-18 21:46:11 +0500

    Completd Section 1 Lesson from 1 to 6

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -7,6 +7,7 @@ namespace CSharpIntermediate
         static void Main(string[] args)
         {
             Console.WriteLine("Star CSharp Intermediate Course!");
+            // Section 1 is About the Course Lessons 1 to 6
         }
     }
 }
