8ec0448c3bbd98f5ab40de290a60bb96e7977a5f
awais <asaeed897>
2020-04-18 21:35:48 +0500

    CSharpIntermediate Project Added

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 
 namespace CSharpBeginner
@@ -9,46 +8,9 @@ namespace CSharpBeginner
     {
         public static void Main(string[] args)
         {
-            var numbers = new List<int> { 1, 2 };
-            var smallests = GetSmallests(numbers, 3);
-
-            foreach (var number in numbers)
-                Console.WriteLine(number);
+            
             // 84. Quiz and Summary CSharp Beginner Course Completed
         }
 
-        public static List<int> GetSmallests(List<int> list, int count)
-        {
-            if (list == null)
-            {
-                throw new ArgumentNullException("list");
-            }
-            if (list.Count < count || count <= 0)
-            {
-                throw new ArgumentOutOfRangeException("Count", "Count should be between 1 and the number of elements in the list.");
-            }
-            var smallests = new List<int>();
-            var buffer = new List<int>(list);
-            while (smallests.Count < count)
-            {
-                var min = GetSmallest(buffer);
-                smallests.Add(min);
-                buffer.Remove(min);
-            }
-
-            return smallests;
-        }
-
-        public static int GetSmallest(List<int> list)
-        {
-            // Assume the first number is the smallest
-            var min = list[0];
-            for (var i = 1; i < list.Count; i++)
-            {
-                if (list[i] < min)
-                    min = list[i];
-            }
-            return min;
-        }
     }
 } 
