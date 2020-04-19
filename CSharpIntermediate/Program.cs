32dd3f96f5105467e686316f72ad760d05de3500
awais <asaeed897>
2020-04-19 18:14:57 +0500

    Completed Lesson 9 Object Initializer

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -7,15 +7,14 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            var customer1 = new Customer(1);
-            var customaer2 = new Customer(2, "Awais");
+            var customer = new Customer() {Name = "Awais", Id = 1};
+            
 
             var order =new Order();
-            customaer2.Orders.Add(order);
+            customer.Orders.Add(order);
 
-            Console.WriteLine("ID: {0}  Name{1}",customaer2.Id,customaer2.Name);
-            // 8 Constructors
+            Console.WriteLine("ID: {0}  Name{1}",customer.Id,customer.Name);
+            // 9 Object Initializer
         }
     }
 }
