393e3bb935d97e192b09de3b1a48cbdfaa7f7f5f
awais <asaeed897>
2020-04-19 19:39:43 +0500

    Completed Lesson 10 Methods

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -7,14 +7,54 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
-            var customer = new Customer() {Name = "Awais", Id = 1};
-            
+            int number;
+            /*try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Conversion failed");
+            }*/
 
-            var order =new Order();
-            customer.Orders.Add(order);
+            // Alternate method by using out Modifier
+            var result = int.TryParse("abc", out number);
+            if (result)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+            // 10 Methods
+        }
 
-            Console.WriteLine("ID: {0}  Name{1}",customer.Id,customer.Name);
-            // 9 Object Initializer
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 4, 5));
+            Console.WriteLine(calculator.Add(1, 2, 4, 5, 5, 6));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+
+        }
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(2, 3);
+                point.Move(10, 20);
+                Console.WriteLine("(X,Y): ({0},{1})", point.X, point.Y);
+
+                point.Move(new Point(5, 10));
+                Console.WriteLine("(X,Y): ({0},{1})", point.X, point.Y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error occured");
+            }
+
         }
     }
 }
