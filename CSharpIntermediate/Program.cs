f79d114853975e356c50b1568938ff3f63740818
awais <asaeed897>
2020-04-19 20:39:16 +0500

    Completed Lesson 12 Access Modifiers

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -3,19 +3,30 @@ using System.Net.Security;
 
 namespace CSharpIntermediate
 {
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
     class Program
     {
         
         static void Main(string[] args)
         {
-           var customer = new Customer(1);
-           
-           customer.Orders.Add(new Order());
-           customer.Orders.Add(new Order());
+           var person = new Person();
+           person.SetBirthdate(new DateTime(1997,01,22));
+           Console.WriteLine(person.GetBirthdate());
 
-           Console.WriteLine(customer.Orders.Count);
-
-            // 11 Fields
+            // 12 Access Modifiers
         }
 
         
