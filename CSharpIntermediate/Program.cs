0e3e28e9752b61daf308998b8b12df4988cb2158
awais <asaeed897>
2020-04-21 19:39:17 +0500

    Completed Lesson 22 Access Modifiers

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,17 +1,28 @@
 ﻿using System.Data;
+using Amazon;
 
 namespace CSharpIntermediate
 {
+    /*public class GoldCustomer : Customer // This class I use to check Protected Access Modifier
+    {
+        public void OfferVouchar()
+        {
+            var rating = this.CalculatingRating(true);
+        }
+    }*/
+
     class Program
     {
 
         static void Main(string[] args)
         {
-            /*Inheritance IS-A Relationship
-             Composition Has-A Relationship*/
+            var customer = new Customer();
+            //Amazon.RateCalculator calculator = new RateCalculator(); // Now I change this class in Amazon Assembly then this file get
+                                                                     // effected too. So In other assemblies where we want to encapsulate 
+                                                                     // one class from the classes on other libraries we use Internal Modifier
 
 
-            // 20 Class Favor Composition Over Inheritance
+            // 22 Access Modifiers Public Private Protected internal and Protected internal
         }
     }
 }
