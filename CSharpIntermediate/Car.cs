3d8eb2b8dc1bb3a2b0826e9443d9d939996d7292
awais <asaeed897>
2020-04-21 20:07:38 +0500

    Completed Lesson 23 Constructors and Inheritance

---
+++ CSharpIntermediate/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+            Console.WriteLine("Car is being initialized.");
+        }
+
+        public Car(string registrationNumber)
+            :base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized {0}",registrationNumber);
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,28 +1,16 @@
-﻿using System.Data;
-using Amazon;
+﻿
 
 namespace CSharpIntermediate
 {
-    /*public class GoldCustomer : Customer // This class I use to check Protected Access Modifier
-    {
-        public void OfferVouchar()
-        {
-            var rating = this.CalculatingRating(true);
-        }
-    }*/
-
     class Program
     {
 
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            //Amazon.RateCalculator calculator = new RateCalculator(); // Now I change this class in Amazon Assembly then this file get
-                                                                     // effected too. So In other assemblies where we want to encapsulate 
-                                                                     // one class from the classes on other libraries we use Internal Modifier
+           var car = new Car("ABC123");
+            
 
-
-            // 22 Access Modifiers Public Private Protected internal and Protected internal
+            // 23 Constructors and Inheritance
         }
     }
 }
