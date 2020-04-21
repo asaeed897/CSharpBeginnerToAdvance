aff72d49502022d25f0a43490b4fd6eecfc4db4b
awais <asaeed897>
2020-04-21 20:56:51 +0500

    Completed Lesson 24 UpCasting and DownCasting

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System;
+
 namespace CSharpIntermediate
 {
     class Program
@@ -7,10 +9,32 @@ namespace CSharpIntermediate
 
         static void Main(string[] args)
         {
-           var car = new Car("ABC123");
-            
+            /*var text = new Text(); //   UpCasting 
+            Shape shape = text;
 
-            // 23 Constructors and Inheritance
+            text.Width = 100;
+            shape.Width = 200;
+
+            Console.WriteLine(text.Width);*/
+
+            var shape = new Shape();
+            var text = (Text) shape; // DownCasting but it is not safe because it can throw an exception 
+            var text1 = shape as Text; // This is the safe method because if it can't type cast then it return null
+            if (text1 != null)
+            {
+                //something
+            }
+            {
+                
+            }
+            // Note: "IS" is keyword which we can use to check the type of object
+            if (text1 is Text)
+            {
+                Console.WriteLine("Object has type Text");
+            }
+
+
+            // 24 UpCasting and DownCasting
         }
     }
 }
