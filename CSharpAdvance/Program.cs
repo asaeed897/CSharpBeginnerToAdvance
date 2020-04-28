2c6fc4ee231abdeb821d09fece1f56e7f93caad8
awais <asaeed897>
2020-04-28 17:06:57 +0500

    Completed Lesson 15 Asynchronous Programming With Async and Await

--- CSharpAdvance/Program.cs
+++ CSharpAdvance/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace CSharpAdvance
 {
@@ -8,29 +10,10 @@ namespace CSharpAdvance
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var number = 5;
-                //var result = number / 0;
-                throw new Exception("Oops.");
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                throw new MyException("Do not divide any number with zero.", e);
-            }
-            finally
-            {
-                // if we have some StreamReader variables or open file handler which CLR didn't dispose
-                // we dispose them here manually
-                // but USING a 'using block' is better choice. it dispose those variable automatically
-            }
-            // 14. Exception Handling
+            Console.WriteLine("Task Completed..");
+            // 15. Asynchronous Programming With Async and Await
         }
-
+        
+        
     }
 }   
